7c81cf1367dcec302f3e9a90efb77a151b78b167
makbn <mehdi74akbarian@gmail.com>
2018-07-05 15:47:36 +0430

    + Constants for const and default value added

---
+++ src/main/java/ir/ac/um/ce/projectnews/utils/Constants.java
@@ -0,0 +1,7 @@
+package ir.ac.um.ce.projectnews.utils;
+
+public class Constants {
+    public static final String USERNAME = "Tasnimnews_Fa";
+    public static final String CSV_PATH = "./";
+    public static final int DEFAULT_MAX_TWEETS=100000;
+}
